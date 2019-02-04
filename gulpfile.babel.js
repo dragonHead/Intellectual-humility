10807d8eae189e9d7f5edbdf38f1774d2799ee74
jack-8 <yasutaka.kouno@gmail.com>
2019-02-04 21:43:21 +0900

    add gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,26 @@
+
+import { gulp, src, dest, series, parallel } from 'gulp';
+import htmlmin from 'gulp-htmlmin';
+import del from 'del';
+
+const paths = {
+  srcDir: './src',
+  distDir: './docs',
+};
+
+export const clean = () => del([`${paths.distDir}/**`, '!dist'], { force: true });
+
+export function html() {
+  return src(`${paths.srcDir}/**/*.html`)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest(paths.distDir));
+}
+
+const build = series(
+  clean,
+  parallel(
+    html,
+  ),
+);
+
+export default build;
