410fdb7b1f6889bc9a162d8f438db653c7899c70
jack-8 <himawari888ty@gmail.com>
2020-02-15 05:53:09 +0900

    update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,9 @@ hello();
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('./serviceWorker.js').then(registration => {
-        console.log('SW registered: ', registration);
+        console.debug('SW registered: ', registration);
       }).catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
+        console.debug('SW registration failed: ', registrationError);
       });
     });
 }
