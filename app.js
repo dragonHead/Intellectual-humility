1f4f299856424c7c53b336b6dd1d1d22ad219a5b
jack-8 <dorayaki08080@gmail.com>
2021-06-26 01:11:27 +0900

    add express

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.use('/monaka', express.static('docs'))
+
+const server = app.listen(port, () => {
+  const h = server.address().address;
+  const p = server.address().port;
+  console.log(`App listening at http://${h}:${p}`);
+});
